219bb2900f4d35fdb84a76c5829b499a21876407
Tom37000 <tom.moukala@gmail.com>
2025-03-07 21:22:39 +0100

    leaderboard ankou cup

--- src/LeaderboardStizoCup.js
+++ src/LeaderboardStizoCup.js
@@ -2,9 +2,9 @@ import './App.css';
 import React, {useState, useEffect} from "react"
 import { useLocation } from 'react-router-dom';
 
-function Row({rank, teamname, points, elims, avg_place, wins}) {
+function Row({rank, teamname, points, elims, avg_place, wins, order}) {
     return (
-        <div className='row_container'>
+        <div className='row_container' style={{ '--animation-order': order }}>
             <div className='rank_container'>{rank}</div>
             <div className='name_container'>{teamname}</div>
             <div className='info_box'>{avg_place.toFixed(2)}</div>  
@@ -20,7 +20,8 @@ function LeaderboardReddyshRoyale() {
     const leaderboard_id = new URLSearchParams(useLocation().search).get('id');
 
     const [leaderboard, setLeaderboard] = useState(null)
-    const [page, setPage] = useState([0, 10])
+    const [page, setPage] = useState([0, 8]) // Changer le nombres d'équipes par page
+    const [teamsPerPage, setTeamsPerPage] = useState(8) // Changer le nombres d'équipes par page
 
     useEffect(() => {
         fetch("https://api.wls.gg/v5/leaderboards/"+leaderboard_id)
@@ -40,16 +41,16 @@ function LeaderboardReddyshRoyale() {
             console.log(data)
             setLeaderboard(leaderboard_list)
         })
-    }, [])
+    }, [leaderboard_id])
 
     function nextPage(){
-        setPage(page.map(num => num + 10))
+        setPage([page[0] + teamsPerPage, page[1] + teamsPerPage])
     }
 
     function previousPage(){
-        setPage(page.map(num => num - 10 ))
+        setPage([page[0] - teamsPerPage, page[1] - teamsPerPage])
     }
-    
+
     return (
         <div className='stizo_cup'> 
             <div className='leaderboard_container'>
@@ -62,11 +63,21 @@ function LeaderboardReddyshRoyale() {
                         <div className='info_header'>WINS</div>
                         <div onClick={nextPage} className='info_header'>POINTS</div>
                     </div>
-                    {leaderboard ? leaderboard.slice(page[0],page[1]).map(data => <Row rank={data.place} teamname={data.teamname} points={data.points} elims={data.elims} wins={data.wins} avg_place={data.avg_place}/>) : ''}
+                    {leaderboard ? leaderboard.slice(page[0],page[1]).map((data, index) => 
+                        <Row 
+                            key={`${page[0]}-${index}`}
+                            rank={data.place} 
+                            teamname={data.teamname} 
+                            points={data.points} 
+                            elims={data.elims} 
+                            wins={data.wins} 
+                            avg_place={data.avg_place}
+                            order={index + 1} 
+                        />
+                    ) : ''}
                 </div>
             </div>
         </div>
-
     )
 }
 
